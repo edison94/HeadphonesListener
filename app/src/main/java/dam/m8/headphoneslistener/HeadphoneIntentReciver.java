b83efcf4c5774df942197603f0a081bc96146631
edison <lrobalino@iesjoandaustria.org>
2017-03-02 20:51:53 +0100

    Comentado el codigo

--- app/src/main/java/dam/m8/headphoneslistener/HeadphoneIntentReciver.java
+++ app/src/main/java/dam/m8/headphoneslistener/HeadphoneIntentReciver.java
@@ -14,53 +14,64 @@ import android.provider.MediaStore;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.NotificationCompat;
 
-public class  HeadphoneIntentReciver extends BroadcastReceiver {
+public class HeadphoneIntentReciver extends BroadcastReceiver {
     private static final int NOTIFICACION_AURICULARES_CONECTADOS = 1;
     private NotificationManager mNotificationManager;
     private NotificationCompat.Builder mBuilder;
+
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equals(Intent.ACTION_HEADSET_PLUG)) {
-            int state = intent.getIntExtra("state", -1);
+        //obtenemos el estado del intent
+        int state = intent.getIntExtra("state", -1);
 
-            switch (state) {
-                case 0:
-                    if (mNotificationManager != null)
-                        mNotificationManager.cancel(NOTIFICACION_AURICULARES_CONECTADOS);
-                    break;
-                case 1:
-                    showNotification("Auriculares conectados",context);
-                    break;
-                default:
-                    showNotification("No creo que esto se muestre",context);
-            }
+        //segun el estado mostraremos la notificacion de que se han conectado los auriculares
+        //o eliminaremos la notificacion si luego de conectarlos los desconecta
+        switch (state) {
+            case 0:
+                if (mNotificationManager != null)
+                    mNotificationManager.cancel(NOTIFICACION_AURICULARES_CONECTADOS);
+                break;
+            case 1:
+                showNotification("Auriculares conectados", context);
+                break;
+            default:
+                showNotification("Error inesperado", context);
         }
     }
 
+    //Metodo en el que crearemos la notificacion
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     private void showNotification(String s, Context context) {
+        //obtenemos el tono de notificacion
         Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        //creamos la notificacion y le añadimos el sonido, vibracion, color de leds y el contenido
         mBuilder =
                 new NotificationCompat.Builder(context)
                         .setSmallIcon(android.R.drawable.stat_sys_headset)
-                        .setLargeIcon((((BitmapDrawable)context.getDrawable(R.drawable.logo)).getBitmap()))
+                        .setLargeIcon((((BitmapDrawable) context.getDrawable(R.drawable.logo)).getBitmap()))
                         .setContentTitle(context.getString(R.string.app_name))
                         .setContentText(s)
-                        .setVibrate(new long[] { 500, 1000 })
+                        .setVibrate(new long[]{500, 1000})
                         .setLights(Color.BLUE, 5000, 5000)
                         .setSound(alarmSound);
+
+        //Creamos un intent para abrir el reproductor de musica
         Intent intent = new Intent(MediaStore.INTENT_ACTION_MEDIA_PLAY_FROM_SEARCH);
 
+        //Creamos un PendingIntent para cuando el usuario haga click en la notificacion se abra el
+        //reproductor de musica
         PendingIntent contIntent =
                 PendingIntent.getActivity(
                         context, 0, intent, 0);
-
+        //Añadimos el PendingIntent a la notificacion
         mBuilder.setContentIntent(contIntent);
 
+        //obtenemos el manager de notificaciones
         mNotificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
+        //Mostramos la notificacion al usuario
         mNotificationManager.notify(NOTIFICACION_AURICULARES_CONECTADOS, mBuilder.build());
     }
 }
--- app/src/main/java/dam/m8/headphoneslistener/ListenerService.java
+++ app/src/main/java/dam/m8/headphoneslistener/ListenerService.java
@@ -16,18 +16,23 @@ public class ListenerService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        //Creamos el Broadcast que vamos a recibir en este caso de conexión de auriculares
         IntentFilter filter = new IntentFilter(Intent.ACTION_HEADSET_PLUG);
+        // Registramos el receptor de conexión de auriculares
         registerReceiver(myReceiver, filter);
-        return START_NOT_STICKY;
+        //Decimos que si el sistema mata el servicio lo vuelva a crear
+        return START_STICKY;
     }
 
     @Override
     public void onDestroy() {
+        //Desregistramos el receptor
         unregisterReceiver(myReceiver);
     }
 
     @Override
     public IBinder onBind(Intent intent) {
+        //Como siempre llamaremos al servicio desde un startService() podemos hacer que este retorne null
         return null;
     }
 }
